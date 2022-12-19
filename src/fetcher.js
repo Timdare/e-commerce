fa443685ccd53821993412a6b4425d13e1b8c6d6
Timdare <timdare12@gmail.com>
2022-12-19 20:46:27 +0100

    changes made to code

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,6 +1,20 @@
 const BASE_URL = "http://localhost:3001"
-export const fetcher = async (url) => {
-    const response = await fetch(BASE_URL + url);
-    const responseData = await response.json();
-    return responseData;
+const fetcher = async (url) => {
+    let responseObject = {"errorMessage": "" , "data": []};
+    try {
+        const response = await fetch(BASE_URL + url);
+        const responseData = await response.json();
+        responseObject.data = responseData;
+    } catch (error) {
+        responseObject.errorMessage = error.message;
+    }
+    return responseObject;
+}
+
+export const getCategories = () => {
+    return fetcher("/categories");
+}
+
+export const getProduct = id => {
+    return fetcher(`/products?catId=${id}`);
 }
