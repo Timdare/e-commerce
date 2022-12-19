ddd24cceb3ca5fdd46bc208dbfd774a06f889f3c
Timdare <timdare12@gmail.com>
2022-12-19 21:49:23 +0100

    changes made to code (Testing Github)

--- src/fetcher.js
+++ src/fetcher.js
@@ -3,12 +3,16 @@ const fetcher = async (url) => {
     let responseObject = {"errorMessage": "" , "data": []};
     try {
         const response = await fetch(BASE_URL + url);
+        if(!response.ok){
+            throw new Error(`HTTP Error ${response.status}. Page not found`);
+        }
         const responseData = await response.json();
         responseObject.data = responseData;
+        return responseObject;
     } catch (error) {
         responseObject.errorMessage = error.message;
+        return responseObject;
     }
-    return responseObject;
 }
 
 export const getCategories = () => {
